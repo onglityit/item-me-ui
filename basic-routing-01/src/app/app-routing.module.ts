7e7e2ad653bf349119d00172f66353fd6c5a095c
Darren Ong <onglityit@entomo.co>
2023-04-28 12:04:02 +0800

    feat(wip): basic routing 1st component

--- basic-routing-01/src/app/app-routing.module.ts
+++ basic-routing-01/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "1st", component: FirstComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [],
 })
 export class AppRoutingModule { }
